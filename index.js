5dbf4c3b11e2ba531e65bd1a65f7ceb2e2c017a4
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2023-03-19 19:59:33 -0400

    save to excel file

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+// todo: hammer down on search feature not being 100% consistent
+// todo: add a confidence meter, when element not found meter goes down.
+// todo: make more consistence on headless.
+// todo: second cluster queue of 5 on 10 queries is giving 'element not found'
+
 const puppeteer = require('puppeteer');
 const { Cluster } = require('puppeteer-cluster');
 var pdfParser = require('pdf-parser');
 const fs = require('fs');
 const { URL } = require('url');
 require('dotenv').config();
+const XLSX = require('xlsx');
 
 const EMAIL = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
 const LOCATION_SEARCH = 'Washington DC';
+const PAGE_SEARCH = 'home buyers';
+const CONCURRENCY = 5;
+const queries = 10;
 
 async function run() {
   const start = Date.now();
@@ -15,8 +24,6 @@ async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1500, height: 900 });
-
-  const queries = 100;
 
   await page.goto('https://facebook.com/login');
 
@@ -55,17 +62,34 @@ async function run() {
   console.log('log in success')
 
   const searchForPage = async () => {
+    await page.waitForTimeout(1000);
     const input = await page.$('input[type="search"]');
-    await input.type('home buyers');
-    await page.waitForTimeout(200);
-    await page.keyboard.press('Enter');
+    await input.type(PAGE_SEARCH);
+    await page.waitForTimeout(100);
     await page.keyboard.press('Enter');
   }
 
-  await searchForPage();
-  await page.waitForNavigation();
-  await page.waitForTimeout(200);
-  console.log('search for page success')
+  const MAX_ATTEMPTS = 3;
+  let attempt = 1;
+  let success = false;
+  
+  while (attempt <= MAX_ATTEMPTS && !success) {
+    try {
+      await searchForPage();
+      await page.waitForNavigation();
+      await page.waitForTimeout(200);
+      console.log('search for page success');
+      success = true;
+    } catch (error) {
+      console.log(`Attempt ${attempt} failed: ${error}`);
+      attempt++;
+    }
+  }
+  
+  if (!success) {
+    console.log(`Search for page failed after ${MAX_ATTEMPTS} attempts.`);
+  }
+  
 
   await page.waitForSelector('div[role="listitem"]');
 
@@ -114,26 +138,94 @@ async function run() {
   };
 
   await searchLocation();
-
   console.log('search location success')
 
   const phoneRegex = /\(\d{3}\) \d{3}-\d{4}/;
   const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i;
-  const websiteRegex = /(?<=")[\w.+(?=")[^\s./]+\.com(?=\"|\s)|(^|\s)[^\s./]+\.com(?!\S|$)/gi;
 
-  const arr = [];
+  // todo better regex this one has false positives for emails
+  const websiteRegex = /(https?:\/\/)?(www\.)?[\w-]+\.(com|org|net|us)/gi
+
+  const pagesData = [];
   const urlsArr = [];
 
-  await page.waitForTimeout(100);
+  await page.waitForTimeout(1500);
 
   if (queries > 10) await pageScroller();
+
+  const tabBack = async () => {
+    await page.waitForTimeout(1000);
+    await page.keyboard.down("Shift");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.up("Shift");
+    await page.waitForTimeout(300);
+  };
+
+  const tab = async () => {
+    await page.waitForTimeout(100);
+    await page.keyboard.press("Tab");
+    await page.waitForTimeout(100);
+    let wrongElFocused = false;
+    let href = "";
+
+    href = await page.evaluate(() => {
+      const focusedElement = document.activeElement;
+      const pfpEl =
+        focusedElement.getAttribute("href")?.includes("https://www.facebook.com/") &&
+        focusedElement.getAttribute("role") === "link" &&
+        !focusedElement.getAttribute("aria-label").includes("ollow") &&
+        !focusedElement.getAttribute("aria-label").includes("ike");
+      if (pfpEl) {
+        return focusedElement.getAttribute("href");
+      }
+    });
+
+    if (href) urlsArr.push(href);
+
+    try {
+      wrongElFocused = await page.evaluate(() => {
+        const focusedElement = document.activeElement;
+
+        const isPfp =
+          focusedElement.getAttribute("role") === "link" &&
+          focusedElement.getAttribute("aria-label") !== "Following" &&
+          focusedElement.getAttribute("href")?.includes("https://www.facebook.com/") ||
+          focusedElement.getAttribute("href")?.includes("/stories/");
+        const verified = focusedElement.getAttribute("aria-label") === "Verified";
+        const isBtn =
+          focusedElement.getAttribute("aria-label")?.includes("ollow") ||
+          focusedElement.getAttribute("aria-label")?.includes("ike");
+        const newMsg = document.querySelector('div[role="button"][aria-label="New Message"]');
+        const isMsg = focusedElement === newMsg;
+
+        if (isMsg || (!isPfp && !verified && !isBtn)) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    if (wrongElFocused) {
+      console.log("focused out of bounds");
+      await tabBack();
+    }
+    await page.waitForTimeout(100);
+  };
+
+  while (urlsArr.length < queries) {
+    await tab();
+  }
+
 
   const PDF_PATH = 'page.pdf';
 
   const generatedPdfs = [];
 
   const scrapePage = async (url, i) => {
-    console.log('page + ' + i + 1)
+    console.log('page ' + (i + 1))
     console.log(url)
 
     // Create a new page instance
@@ -194,12 +286,13 @@ async function run() {
     if (phoneNumber) obj.phoneNumber = phoneNumber;
     if (email) obj.email = email;
     if (website) obj.website = website;
-    if (pageTitle) obj.pageTitle = pageTitle;
+    if (pageTitle) obj.pageTitle = pageTitle.replace(' | Facebook', '');
+    
     const parsedUrl = new URL(url).toString().replace('__tn__=%3C', "");
     obj.url = parsedUrl;
 
     if (Object.keys(obj).length) {
-      arr.push(obj);
+      pagesData.push(obj);
       await newPage.close();
     } else {
       console.log("no data found");
@@ -208,71 +301,6 @@ async function run() {
     await newPage.close();
   };
 
-
-  const tabBack = async () => {
-    await page.waitForTimeout(1000);
-    await page.keyboard.down("Shift");
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
-    await page.keyboard.up("Shift");
-    await page.waitForTimeout(300);
-  };
-
-  const tab = async () => {
-    await page.keyboard.press("Tab");
-    let wrongElFocused = false;
-    let href = "";
-
-    href = await page.evaluate(() => {
-      const focusedElement = document.activeElement;
-      const pfpEl =
-        focusedElement.getAttribute("href")?.includes("https://www.facebook.com/") &&
-        focusedElement.getAttribute("role") === "link" &&
-        !focusedElement.getAttribute("aria-label").includes("ollow") &&
-        !focusedElement.getAttribute("aria-label").includes("ike");
-      if (pfpEl) {
-        return focusedElement.getAttribute("href");
-      }
-    });
-
-    if (href) urlsArr.push(href);
-
-    try {
-      wrongElFocused = await page.evaluate(() => {
-        const focusedElement = document.activeElement;
-
-        const isPfp =
-          focusedElement.getAttribute("role") === "link" &&
-          focusedElement.getAttribute("aria-label") !== "Following" &&
-          focusedElement.getAttribute("href")?.includes("https://www.facebook.com/") ||
-          focusedElement.getAttribute("href")?.includes("/stories/");
-        const verified = focusedElement.getAttribute("aria-label") === "Verified";
-        const isBtn =
-          focusedElement.getAttribute("aria-label")?.includes("ollow") ||
-          focusedElement.getAttribute("aria-label")?.includes("ike");
-        const newMsg = document.querySelector('div[role="button"][aria-label="New Message"]');
-        const isMsg = focusedElement === newMsg;
-
-        if (isMsg || (!isPfp && !verified && !isBtn)) {
-          return true;
-        } else {
-          return false;
-        }
-      });
-    } catch (err) {
-      console.log(err);
-    }
-    if (wrongElFocused) {
-      console.log("focused out of bounds");
-      await tabBack();
-    }
-    await page.waitForTimeout(100);
-  };
-
-  while (urlsArr.length < queries) {
-    await tab();
-  }
-
   const scrapeArrURL = async (url, page, i) => {
     await page.waitForTimeout(100);
     await scrapePage(url, i);
@@ -280,7 +308,10 @@ async function run() {
 
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_PAGE,
-    maxConcurrency: 5,
+    maxConcurrency: CONCURRENCY,
+    puppeteerOptions: {
+      headless: true,
+    }
   });
 
   await cluster.task(async ({ page, data: { url, i} }) => {
@@ -297,11 +328,19 @@ async function run() {
   const end = Date.now();
   const elapsed = end - start;
   const timeTaken = `Function took ${elapsed} milliseconds to complete ${queries} pages.`;
-  console.log(arr)
+  console.log(pagesData)
   console.log(urlsArr)
   console.log(timeTaken)
 
-  const data = arr.map(obj => JSON.stringify(obj)).join('\n') + '\n' + timeTaken;
+  const data = pagesData.map(obj => JSON.stringify(obj)).join('\n') + '\n' + timeTaken;
+
+  const headers = ["phone number", "email", "website", "page title", "url"];
+
+  const wb = XLSX.utils.book_new();
+  const ws = XLSX.utils.json_to_sheet(pagesData, { headers });
+
+  XLSX.utils.book_append_sheet(wb, ws, "Data");
+  XLSX.writeFile(wb, 'data.xlsx');
 
   for (const pdf of generatedPdfs) {
     fs.unlink(pdf, (err) => {
@@ -309,7 +348,21 @@ async function run() {
     });
   };
 
-  fs.writeFile('results101.txt', data, function (err) {
+  let i = 0;
+  let filename = `results/results${i}.txt`;
+  while (true) {
+    try {
+      fs.accessSync(filename);
+      i++;
+      filename = `results/results${i}.txt`;
+    } catch (error) {
+      break;
+    }
+  }
+  console.log(`Using filename: ${filename}`);
+  
+
+  fs.writeFile(filename, data, function (err) {
     if (err) {
       console.log('Error saving file:', err);
     } else {
