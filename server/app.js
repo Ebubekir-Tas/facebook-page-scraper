00c0fc1835aa9dd9768059ef9436a5a01fcd5c22
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2023-03-20 16:29:38 -0400

    init scrape post req

--- server/app.js
+++ server/app.js
@@ -1,19 +1,26 @@
-
 const express = require('express');
 const scraper = require('./scraper');
-  
+const cors = require('cors');
+
 const app = express();
 const PORT = 8000;
-  
-app.get('/scrape', async (req, res) => {
-  const data = await scraper.run();
-  res.json(data);
+
+app.use(cors());
+
+
+app.post('/scrape', async (req, res) => {
+  try {
+    const result = await scraper.run();
+    res.json(result);
+  } catch (error) {
+    console.error('Error running function:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+  if(!error)
+    console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+  else 
+    console.log("Error occurred, server can't start", error);
+});
